9aad4df9b28d0bc144dd3b15c7d27dbd3ebbe766
DarkA189 <rushik.more189@gmail.com>
2022-10-11 13:55:13 +0530

    Second Commit

--- docscanner/models.py
+++ docscanner/models.py
@@ -1,3 +1,7 @@
+from operator import mod
 from django.db import models
 
 # Create your models here.
+'''class document_scanner_storage(models.Model):
+    key = models.CharField(maxlength=255,default=0)
+    image = models.ImageField(upload_to="docscan/", default="")'''
--- docscanner/urls.py
+++ docscanner/urls.py
@@ -5,8 +5,9 @@ from docscanner import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('texthand', views.texthand, name='texthand'),
-    path('texthand/process/', views.processHandtoText, name='texthand'),
-    
+    path('texthand/process/', views.processHandtoText, name='texthandprocess'),
+    path('docscan', views.scanDocument, name='docscan'),
+    path('docscan/process/', views.scanDocumentProcess, name='docscanprocess'),
     
     
 ]
